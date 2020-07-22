aeac5ad14f9f2dab3808338c2e36e64efd16788a
nozoyama39 <nozoyama39@gmail.com>
2020-07-22 09:13:42 +0900

    implemented ETA in progress bar

--- main.cpp
+++ main.cpp
@@ -7,9 +7,10 @@
 using namespace std;
 
 int main() {
-	Grouping gp("./testcase/form.in");
-	gp.global_search(10, 10000);
-	gp.print_pairs();
+    Grouping gp("./testcase/form.in");
+    // Grouping gp("./testcase/sample.in");
+    gp.global_search(100, 10000);
+    gp.print_pairs();
 
-	return 0;
+    return 0;
 }
